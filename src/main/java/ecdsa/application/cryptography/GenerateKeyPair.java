7c151a40baf39b440a9fbb519c96fbd789e9b183
kareltan2 <petrus.kanisius.karel.tan@gmail.com>
2023-12-22 06:59:30 +0700

    prepare cryptography

--- src/main/java/ecdsa/application/cryptography/GenerateKeyPair.java
+++ src/main/java/ecdsa/application/cryptography/GenerateKeyPair.java
@@ -7,19 +7,29 @@ import static ecdsa.application.constant.CommonConstant.EC;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
+import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
 
 /**
  * @author kareltan
  */
-public class GenerateKeyPair {
+@Slf4j
+public class GenerateKeyPair extends ECDSACryptographyAbstract {
 
   public KeyPair generateKeyPair() throws Exception {
+    addProvider();
+
+    log.info("Starting generate key pair with curve: {}", CURVE);
     ECNamedCurveParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec(CURVE);
     KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(EC, BC);
     keyPairGenerator.initialize(ecSpec, new SecureRandom());
-    return keyPairGenerator.generateKeyPair();
+
+    KeyPair keyPairGenerated = keyPairGenerator.generateKeyPair();
+    log.info("Successfully generated key pair with value, privateKey: {}, publicKey: {}",
+        keyPairGenerated.getPrivate().getEncoded(), keyPairGenerated.getPublic().getEncoded());
+
+    return keyPairGenerated;
   }
 
 }
