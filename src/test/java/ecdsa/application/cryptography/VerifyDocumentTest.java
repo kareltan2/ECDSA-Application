69a43865603c0e2462d6474c2cd54ece16189310
kareltan <karel.tan@tiket.com>
2024-01-18 03:16:37 +0700

    add test case for verify document

--- src/test/java/ecdsa/application/cryptography/VerifyDocumentTest.java
+++ src/test/java/ecdsa/application/cryptography/VerifyDocumentTest.java
@@ -18,7 +18,7 @@ public class VerifyDocumentTest extends AbstractTestingVariable {
   private final GenerateKeyPair generateKeyPair = new GenerateKeyPair();
 
   @Test
-  void testSignDocument_shouldSuccess() throws Exception{
+  void testVerifyDocument_shouldSuccess() throws Exception{
     KeyPair keyPair = generateKeyPair.generateKeyPair();
     PrivateKey privateKey = keyPair.getPrivate();
     PublicKey publicKey = keyPair.getPublic();
@@ -27,6 +27,51 @@ public class VerifyDocumentTest extends AbstractTestingVariable {
     boolean isVerified = verifyDocument.verifySignature(data, signature, publicKey);
 
     Assertions.assertTrue(isVerified);
+    Assertions.assertNotNull(signature);
+    Assertions.assertNotNull(keyPair);
+  }
+
+  @Test
+  void testVerifyDocument_withPublicKeyNotValid_resultWillNotValid_shouldSuccess() throws Exception{
+    KeyPair keyPair = generateKeyPair.generateKeyPair();
+    KeyPair keyPair2 = generateKeyPair.generateKeyPair();
+
+    PrivateKey privateKey1 = keyPair.getPrivate();
+    PublicKey publicKey2 = keyPair2.getPublic();
+
+    byte[] signature = signDocument.signData(data, privateKey1);
+
+    boolean isVerified = verifyDocument.verifySignature(data, signature, publicKey2);
+
+    Assertions.assertFalse(isVerified);
+    Assertions.assertNotNull(signature);
+    Assertions.assertNotNull(keyPair);
+  }
+
+  @Test
+  void testVerifyDocument_withDifferentOriginalFile_resultWillNotValid_shouldSuccess() throws Exception{
+    KeyPair keyPair = generateKeyPair.generateKeyPair();
+    PrivateKey privateKey = keyPair.getPrivate();
+    PublicKey publicKey = keyPair.getPublic();
+    byte[] signature = signDocument.signData(data, privateKey);
+
+    boolean isVerified = verifyDocument.verifySignature(data.concat("123"), signature, publicKey);
+
+    Assertions.assertFalse(isVerified);
+    Assertions.assertNotNull(signature);
+    Assertions.assertNotNull(keyPair);
+  }
+
+  @Test
+  void testVerifyDocument_withDifferentSignedFile_resultWillNotValid_shouldSuccess() throws Exception{
+    KeyPair keyPair = generateKeyPair.generateKeyPair();
+    PrivateKey privateKey = keyPair.getPrivate();
+    PublicKey publicKey = keyPair.getPublic();
+    byte[] signature = signDocument.signData(data.concat("123"), privateKey);
+
+    boolean isVerified = verifyDocument.verifySignature(data, signature, publicKey);
+
+    Assertions.assertFalse(isVerified);
     Assertions.assertNotNull(signature);
     Assertions.assertNotNull(keyPair);
   }
